15ed4ba85ea1d98c5409c5b84f1ce7b14d373a96
Nishant <Nishantraj87726@gmail.com>
2023-07-18 01:04:43 +0530

    register done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Header from './components/Layout/Header/Header'
 import Courses from './components/Courses/Courses'
 import Footer from './components/Layout/Footer/Footer'
 import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
 
 const App = () => {
   return (
     <Router>
-    <Header />
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/courses' element={<Courses />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/components/Auth/Register.jsx
@@ -0,0 +1,121 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export const fileUploadCss = {
+  cursor: 'pointer',
+  marginLeft: '-5%',
+  width: '110%',
+  border: 'none',
+  height: '100%',
+  color: '#ecc94b',
+  backgroundColor: '#fff',
+};
+
+const fileUploadStyle = {
+  '&::file-selector-button': fileUploadCss,
+};
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [imagePrev, setImagePrev] = useState();
+  const [image, setImage] = useState('')
+
+  const changeImageHandler = e => {
+    const file = e.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImagePrev(reader.result);
+      setImage(file)
+    };
+  };
+  return (
+    <Container height={'95vh'}>
+      <VStack height={'full'} justifyContent={'center'} spacing={'16'}>
+        <Heading textTransform={'uppercase'}>Registration</Heading>
+        <form action="" style={{ width: '100%' }}>
+          <Box my={'4'} display={'flex'} justifyContent={'center'}>
+            <Avatar src={imagePrev} size={'2xl'} />
+          </Box>
+          <Box my={'4'}>
+            <FormLabel htmlFor="email" children="Name" />
+            <Input
+              required
+              id="name"
+              value={name}
+              onChange={event => setName(event.target.value)}
+              placeholder="John Doe"
+              type={'text'}
+              focusBorderColor="yellow.500"
+            />
+          </Box>
+
+          <Box my={'4'}>
+            <FormLabel htmlFor="email" children="Email Address" />
+            <Input
+              required
+              id="email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+              placeholder="abc@def.com"
+              type={'email'}
+              focusBorderColor="yellow.500"
+            />
+          </Box>
+          <Box my={'4'}>
+            <FormLabel htmlFor="password" children="Password" />
+            <Input
+              required
+              id="password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              placeholder="Enter your Password"
+              type={'password'}
+              focusBorderColor="yellow.500"
+            />
+          </Box>
+          <Box my={'4'}>
+            <FormLabel htmlFor="chooseAvatar" children="Choose Avatar" />
+            <Input
+              accept="image/*"
+              required
+              id="chooseAvatar"
+              type={'file'}
+              focusBorderColor="yellow.500"
+              css={fileUploadStyle}
+              onChange={changeImageHandler}
+            />
+          </Box>
+
+          <Button my={'4'} colorScheme={'yellow'} type="submit">
+            Sign Up
+          </Button>
+          <Box my={'4'}>
+            Already Signed Up?{' '}
+            <Link to="/login">
+              <Button colorScheme={'yellow'} variant={'link'}>
+                Login
+              </Button>{' '}
+              here
+            </Link>
+          </Box>
+        </form>
+      </VStack>
+    </Container>
+  );
+};
+
+export default Register;
